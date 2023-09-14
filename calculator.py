760b02c40160b8fa6f74a7983e8d315f799fdbe3
apexsaksham <141954745+apexsaksham@users.noreply.github.com>
2023-09-14 21:39:36 +0530

    Add files via upload
    This is the internship project for coderscave

---
+++ calculator.py
@@ -0,0 +1,16 @@
+ # ----------------------------Calculator-------------------------------------
+print("+ ADD    - SUBTRACT    * MULTIPLY     / DIVISION" )
+
+num1=int(input("Enter the value:1: "))
+num2=int(input("Enter the value:2: "))
+opr=input(":Enter the opr..(+,-,/,*)---> ")
+if opr == "+":
+  print(num1 + num2)
+elif opr == "-":
+  print(num1 - num2)
+elif opr == "*":
+  print(num1 * num2)
+elif opr == "/":
+  print(num1 / num2)
+if opr!="+" and opr!="-" and opr!="/" and opr!="*":
+  print("Invalid Opr... ")
